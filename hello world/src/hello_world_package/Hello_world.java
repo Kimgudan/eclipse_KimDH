a5dae917c793fa691afda6fe67dfe9a06097d55d
wku <wku@192.168.172.171>
2020-11-03 09:08:00 +0900

    DongHan -> DH

--- hello world/src/hello_world_package/Hello_world.java
+++ hello world/src/hello_world_package/Hello_world.java
@@ -3,6 +3,6 @@ package hello_world_package;
 public class Hello_world {
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		System.out.println("Name : Kim, Dong Han");
+		System.out.println("Name : Kim, DH");
 	}
 }
