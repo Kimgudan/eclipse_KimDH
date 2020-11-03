de5dda78ffce2a9186c64a309afdebab8ef3f60e
wku <wku@192.168.172.171>
2020-11-03 09:31:04 +0900

    create 학번

--- hello world/src/hello_world_package/Hello_world.java
+++ hello world/src/hello_world_package/Hello_world.java
@@ -4,6 +4,6 @@ public class Hello_world {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("Name : Kim, DH");
-
+		System.out.println("ÇÐ¹ø : 20182854");
 	}
 }
