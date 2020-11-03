53506d7d87ec1f2a03b647e17dd4828692cd40e0
wku <wku@192.168.172.171>
2020-11-03 09:31:48 +0900

    create 학과

--- hello world/src/hello_world_package/Hello_world.java
+++ hello world/src/hello_world_package/Hello_world.java
@@ -4,5 +4,6 @@ public class Hello_world {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("Name : Kim, Dong Han");
+		System.out.println("컴퓨터소프트웨어공학과");
 	}
 }
