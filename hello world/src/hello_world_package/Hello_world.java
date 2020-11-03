1876da2c10e214858980c3585469b053759f9639
wku <wku@192.168.172.171>
2020-11-03 09:32:28 +0900

    Dong Han -> DH

--- hello world/src/hello_world_package/Hello_world.java
+++ hello world/src/hello_world_package/Hello_world.java
@@ -3,7 +3,7 @@ package hello_world_package;
 public class Hello_world {
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		System.out.println("Name : Kim, Dong Han");
+		System.out.println("Name : Kim, DH");
 		System.out.println("컴퓨터소프트웨어공학과");
 	}
 }
